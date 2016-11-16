9ce9c91143cc51f19485d03511ec9a3d8133911a
patrickpu <pxlxinzhao@gmail.com>
2016-11-16 17:16:44 -0500

    change title

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -11,6 +11,8 @@ export class AboutPage {
 
   categories:string[] = [];
   records:any = {};
+  idForTitleChanging: string = "";
+  titles: any = {};
   helper:Helper;
 
   constructor(public navCtrl:NavController, helper:Helper) {
@@ -21,6 +23,20 @@ export class AboutPage {
     }
     this.categories = JSON.parse(window.localStorage['categories']);
 
+    this.refresh();
+  }
+
+  test(id){
+    console.log(this.titles[id]);
+  }
+
+  setTitleKey(id){
+    this.idForTitleChanging = id;
+  }
+
+  changeTitle(){
+    this.helper.update('records', this.idForTitleChanging, 'title', this.titles[this.idForTitleChanging]);
+    this.idForTitleChanging = "";
     this.refresh();
   }
 
