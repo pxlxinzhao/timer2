72c7e21f138b57c5e6927d219b77c2ff0d56daf1
Patrick Pu <pxlxinzhao@gmail.com>
2017-02-12 11:01:27 -0500

    made power cell calculation

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -18,10 +18,16 @@ export class CalendarPage {
    */
   calendarMap: Object = {};
 
+  /**
+   * 4 power cell only have 3 levels
+   */
+  level1: any;
+  level2: any;
+  level3: any;
+
   constructor(private pouch: Pouch){
     let records = JSON.parse(this.pouch.getLocal("records"));
 
-    //console.log("records", records);
     /**
      * records are already sorted
      */
@@ -38,13 +44,41 @@ export class CalendarPage {
         this.calendarMap[dateStr].totalTime += record.doc.duration/1;
         this.calendarMap[dateStr].count += 1;
       }
-
-      //console.log(this.calendarMap);
     }
+
+    /**
+     * calculate threshold for power cell
+     */
+    let durationArray = [];
+
+    for (let key in this.calendarMap){
+      durationArray.push(this.calendarMap[key].totalTime);
+    }
+
+    durationArray.sort((a, b)=>{return a-b});
+    console.log('durationArray', durationArray);
+    let length = durationArray.length;
+
+    let threshold1 = Math.floor(length/4);
+    let threshold2 = Math.floor(length/4 * 2);
+    let threshold3 = Math.floor(length/4 * 3);;
+
+    this.level1 = durationArray[threshold1];
+    this.level2 = durationArray[threshold2];
+    this.level3 = durationArray[threshold3];
+
+    console.log('this.level1', this.level1);
+    console.log('this.level2', this.level2);
+    console.log('this.level3', this.level3);
   }
 
   getDate(record){
     return moment(record.doc.timestamp/1).format('MMM DD YYYY');
+  }
+
+  getLevel(duration){
+    let result = duration >= this.level3 ? 3 : duration >= this.level2 ? 2 : duration >= this.level1 ? 1 : 0;
+    return result;
   }
 
 }
